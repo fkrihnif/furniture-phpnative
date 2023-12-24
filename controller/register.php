70db7501b5faa6d1c9421a82d3ac327d4a3f6459
Fikri Hanif <fikrihanif77@gmail.com>
2023-12-24 16:26:58 +0700

    fixing bug register

--- controller/register.php
+++ controller/register.php
@@ -11,36 +11,50 @@ $konfirmasi = $_POST['konfirmasi'];
 $hash = password_hash($password, PASSWORD_DEFAULT);
 
 if($password == $konfirmasi){
-	$cek = mysqli_query($conn, "SELECT username from customer where username = '$username'");;
+	$cek = mysqli_query($conn, "SELECT username from customer where username = '$username'");
 	$jml = mysqli_num_rows($cek);
 
+	$cek_email = mysqli_query($conn, "SELECT email from customer where email = '$email'");
+	$jml_email = mysqli_num_rows($cek_email);
 	if($jml == 1){
 		echo "
 		<script>
 		alert('Username Has Been Used');
-		window.location = '../register.php';
+		window.location.href = '../register.php';
 		</script>
 		";
 		die;
 	}
 
-	$result = mysqli_query($conn, "INSERT INTO customer VALUES(null ,'$nama', '$email', '$username', '$hash')");
+	if($jml_email == 1){
+		echo "
+		<script>
+		alert('Email Has Been Used');
+		window.location.href = '../register.php';
+		</script>
+		";
+		die;
+	}
+
+	$result = mysqli_query($conn, "INSERT INTO customer VALUES(null ,'$username', '$email', '$nama', '$hash')");
 	if($result){
 		echo "
 		<script>
 		alert('Register Success!');
-		window.location = '../login.php';
+		window.location.href = '../login.php';
 		</script>
 		";
+		die;
 	}
 
 }else{
 	echo "
 	<script>
 	alert('Confirm Password Not Same');
-	window.location = '../register.php';
+	window.location.href = '../register.php';
 	</script>
 	";
+	die;
 }
 
 
